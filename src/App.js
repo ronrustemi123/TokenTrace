22b338900cf36d7d8872691d9c587b66e3ff7a9e
Ron Rustemi <rron4348@gmail.com>
2023-09-04 23:26:18 +0200

    arst

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ function App() {
  
       <Routes>
         
-        <Route exact path='/TokenTrace' element=
+        <Route exact path='/' element=
         {<>
           <Navigation />
           <Header />
